fb998f8c7ad74818a2a657cb73e008744e88aecf
hazzard993 <hazzard993@gmail.com>
2019-11-06 19:13:04 +1000

    Remove test files

--- test.lua
+++
@@ -1,4 +0,0 @@
---[[ Generated with https://github.com/TypeScriptToLua/TypeScriptToLua ]]
-local peachy = require("peachy")
-local a = peachy:new("animation.json")
-a:draw(0, 0)
--- test.ts
+++
@@ -1,8 +0,0 @@
-/// <reference no-default-lib="true" />
-/// <reference lib="esnext" />
-/// <reference types="love-typescript-definitions" />
-/// <reference path="./index.d.ts" />
-import * as peachy from "peachy";
-
-const a = peachy.new("animation.json");
-a.draw(0, 0);
